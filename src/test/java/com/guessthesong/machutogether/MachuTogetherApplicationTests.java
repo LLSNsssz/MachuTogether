c09ba498aae6d246d5e5a31269e9b8cf2ef6fba1
LLSNsssz <seungjang93@gmail.com>
2024-08-07 17:29:54 +0900

    Init: 프로젝트 기본 구조 및 설정

---
+++ src/test/java/com/guessthesong/machutogether/MachuTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guessthesong.machutogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachuTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
