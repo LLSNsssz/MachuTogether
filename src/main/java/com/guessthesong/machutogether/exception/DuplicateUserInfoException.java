0e09427daf1feab2c61182f0c42fb0336bc26925
LLSNsssz <seungjang93@gmail.com>
2024-08-17 19:07:55 +0900

    Feat: DuplicateUserInfoException 추가
    - 유저 정보 예외 하나로 통합

---
+++ src/main/java/com/guessthesong/machutogether/exception/DuplicateUserInfoException.java
@@ -0,0 +1,8 @@
+package com.guessthesong.machutogether.exception;
+
+public class DuplicateUserInfoException extends RuntimeException{
+
+    public DuplicateUserInfoException(String message) {
+        super(message);
+    }
+}
