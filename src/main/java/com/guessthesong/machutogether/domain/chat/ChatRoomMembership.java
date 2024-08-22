49cd6c12c6a1d9aafc657b18ee2eade5e3ca6a20
LLSNsssz <seungjang93@gmail.com>
2024-08-22 22:50:27 +0900

    Refactor: UserChatRoom 롬복 어노테이션 수정 및 이름 수정
    - AllArgsConstructor 제거
    - 생성자 추가 후 빌더 어노테이션 추가
    - user 엔티티와 다대일 관계 설정
    - 명시적인 클래스 이름인 ChatRoomMembership 으로 변경

--- src/main/java/com/guessthesong/machutogether/domain/chat/ChatRoomMembership.java
+++ src/main/java/com/guessthesong/machutogether/domain/chat/ChatRoomMembership.java
@@ -1,5 +1,7 @@
 package com.guessthesong.machutogether.domain.chat;
 
+import com.guessthesong.machutogether.domain.user.User;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,28 +11,28 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
 import java.time.Instant;
+
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Builder
-@AllArgsConstructor
+
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
-@Table(name = "user_chatroom")
-public class UserChatRoom {
-
+@Table(name = "chat_room_membership")
+public class ChatRoomMembership {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "user_id", nullable = false)
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "chat_room_id", nullable = false)
@@ -42,10 +44,12 @@ public class UserChatRoom {
     @Column(nullable = false)
     private boolean isAdmin;
 
-    @Column(length = 50)
-    private String nickname;
-
-    @Column(nullable = false)
-    private boolean notificationEnabled;
+    @Builder
+    public ChatRoomMembership(User user, ChatRoom chatRoom, Instant joinedAt, boolean isAdmin) {
+        this.user = user;
+        this.chatRoom = chatRoom;
+        this.joinedAt = joinedAt;
+        this.isAdmin = isAdmin;
+    }
 
 }
